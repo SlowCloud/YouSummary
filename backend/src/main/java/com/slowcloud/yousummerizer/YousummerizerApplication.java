d1f36c3bc5ec2685a8b518a0dbce8cfc6844a826
slowcloud <sju9417@gmail.com>
2024-12-14 17:15:46 +0900

    [init] 백엔드 리포지토리 구성

---
+++ backend/src/main/java/com/slowcloud/yousummerizer/YousummerizerApplication.java
@@ -0,0 +1,13 @@
+package com.slowcloud.yousummerizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YousummerizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YousummerizerApplication.class, args);
+	}
+
+}
